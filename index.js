c959b7010458820810801918cf0ed9b802c5dff1
miguelraad28 <miguelraad2020@gmail.com>
2023-04-23 21:19:27 -0300

    Mobile adaptable

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     var aTags = document.getElementsByClassName("aHoverable");
-    console.log(aTags);
     for (var i = 0; i < aTags.length; i++) {
         (function (i) {
             setTimeout(function () {
                 aTags[i].classList.add("beforeAndAfterInNavbar")
-                console.log(aTags[i])
             }, 310);
         })(i);
     }
